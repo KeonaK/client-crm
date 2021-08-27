590bb501c8df0caec1d4b20c5db3b1dcb7c75909
Keona Kirby <keonakirby@gmail.com>
2021-08-26 21:28:43 -0500

    added current time to the dashboard header. Needs css for styling

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let dateDisplayEl = document.getElementById("dashboardDate");
+const dateDisplayEl = document.getElementById("dashboardDate");
+const timeDisplayEl = document.getElementById("dashboardTime");
+
+const DateTime = luxon.DateTime;
 
 function todaysDate() {
-  let DateTime = luxon.DateTime;
   let today = DateTime.now().toFormat("MMMM dd, yyyy");
   dateDisplayEl.textContent = today;
 }
 
 todaysDate();
+
+function currentTime() {
+  const dt = DateTime.local();
+  let time = dt.toLocaleString(DateTime.TIME_SIMPLE);
+  timeDisplayEl.textContent = time;
+}
+
+currentTime();
