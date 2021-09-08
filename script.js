a881279f3c649133d955c0fccd10431f0b96a7d3
Keona Kirby <keonakirby@gmail.com>
2021-09-07 21:08:18 -0500

    created function to get data from form and add to local storage but it is not currently working

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const dateDisplayEl = document.getElementById("dashboardDate");
 const timeDisplayEl = document.getElementById("dashboardTime");
 
+const btnEl = document.getElementById("btn");
+
 const DateTime = luxon.DateTime;
 
+//display the current date and time
 function todaysDate() {
   let today = DateTime.now().toFormat("MMMM dd, yyyy");
   dateDisplayEl.textContent = today;
@@ -17,3 +20,26 @@ function currentTime() {
 }
 
 currentTime();
+
+//submitting the form
+
+let tasks = [];
+
+const addTask = (event) => {
+  event.preventDefault(); // prevents the form from submitting and reloading the page
+  let task = {
+    id: Date.now(),
+    priority: document.getElementById("priority").value,
+    date: document.getElementById("date").value,
+    taskCreated: document.getElementById("taskCreated").value,
+    status: document.getElementById("status").value,
+  };
+
+  tasks.push(task); //add the new task to our tasks object
+  document.querySelector("form").reset(); //clears the form for the next task
+
+  //turns object into a string to add it to local storage
+  localStorage.setItem("taskList", JSON.stringify(tasks));
+};
+
+btnEl.addEventListener("click", addTask);
